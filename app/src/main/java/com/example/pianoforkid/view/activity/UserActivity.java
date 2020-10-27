948ccc722e1d59ad1a333d08a35d0e03eb289cf6
ntmlinh <hoadaomylinh@gmail.com>
2020-10-27 21:56:43 +0700

    done sign in + sign out

--- app/src/main/java/com/example/pianoforkid/view/activity/UserActivity.java
+++ app/src/main/java/com/example/pianoforkid/view/activity/UserActivity.java
@@ -1,13 +1,13 @@
 package com.example.pianoforkid.view.activity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.pianoforkid.R;
@@ -17,25 +17,23 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String ANONYMOUS = "anonymous";
     //*****************
     private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mMessageDatabaseReference;
-    private ChildEventListener mChildEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private String mUsername;
     User userX;
-    TextView email;
+    TextView txt_email;
+    TextView txt_user_name;
+    TextView txt_score;
+    Button btn_sign_out;
+    Button btn_back;
 
     // Choose an arbitrary request code value
     private static final int RC_SIGN_IN = 1;
@@ -52,23 +50,16 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
 
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
-        email = findViewById(R.id.text_view_username);
+        txt_email = findViewById(R.id.txt_email);
+        txt_user_name = findViewById(R.id.txt_user_name);
+        txt_score = findViewById(R.id.txt_score);
+        btn_sign_out = findViewById(R.id.btn_sign_out);
+        btn_back = findViewById(R.id.btn_back);
 
-        userViewModel.getUser().observe(this, u ->
-        {
-            userX = u;
-            try {
-                email.setText(u.identifier);
-            }
-            catch (Exception e){
-                System.out.println(e);
-            }
-        });
         mUsername = ANONYMOUS;
         //*****************
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
-        mMessageDatabaseReference = mFirebaseDatabase.getReference().child("message");
         //*****************
 
         mAuthStateListener = firebaseAuth -> {
@@ -76,6 +67,18 @@ public class UserActivity extends AppCompatActivity {
             if(user != null){
                 //sign in
                 onSignInInitialize(user.getDisplayName());
+                userViewModel.getUser().observe(this, u ->
+                {
+                    userX = u;
+                    try {
+                        txt_email.setText(u.identifier);
+                        txt_user_name.setText(u.name);
+                        txt_score.setText(u.score);
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
+                });
             }else{
                 //sign out
                 onSignedOutCleanUp();
@@ -90,61 +93,21 @@ public class UserActivity extends AppCompatActivity {
                                 .build(),
 
                         RC_SIGN_IN);
-
-
             }
         };
         //*****************
-
+        btn_sign_out.setOnClickListener(this);
+        btn_back.setOnClickListener(this);
     }
     private void onSignInInitialize(String username){
         mUsername = username;
-        attachDatabaseReadListener();
     }
 
     private void onSignedOutCleanUp(){
         mUsername = ANONYMOUS;
-        //mMessageAdapter.clear();
-    }
-    private void attachDatabaseReadListener(){
-        if(mChildEventListener == null) {
-
-            mChildEventListener = new ChildEventListener() {
-                @Override
-                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                   /* FriendlyMessage friendlyMessage = snapshot.getValue(FriendlyMessage.class);
-                    mMessageAdapter.add(friendlyMessage);*/
-                }
-
-                @Override
-                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                }
-
-                @Override
-                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            };
-            mMessageDatabaseReference.addChildEventListener(mChildEventListener);
-        }
     }
 
-    private void detachDatabaseReadListener(){
-        if(mChildEventListener != null) {
-            mMessageDatabaseReference.removeEventListener(mChildEventListener);
-        }
-    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -155,7 +118,6 @@ public class UserActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-        detachDatabaseReadListener();
     }
 
     @Override
@@ -168,7 +130,7 @@ public class UserActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                userViewModel.insertUser(new User(user.getUid(), user.getDisplayName(), user.getEmail(), 0));
+                userViewModel.insertUser(new User(user.getUid(), user.getDisplayName(), user.getEmail(), 200));
                 // ...
             } else {
                 // Sign in failed. If response is null the user canceled the
@@ -179,4 +141,22 @@ public class UserActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_sign_out:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(task -> {
+                            txt_email.setText("");
+                            txt_user_name.setText("");
+                            txt_score.setText("");
+                            MainMenuActivity.startActivity(this);
+                        });
+                break;
+            case R.id.btn_back:
+                onBackPressed();
+                break;
+        }
+    }
 }
