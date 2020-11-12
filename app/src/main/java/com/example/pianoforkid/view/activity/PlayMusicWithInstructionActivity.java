7ab2e2f56fcda5ec4efacedb8ba8f20ca0acfc28
ntmlinh <hoadaomylinh@gmail.com>
2020-11-12 21:44:32 +0700

    Update PlayMusicWithInstructionActivity.java

--- app/src/main/java/com/example/pianoforkid/view/activity/PlayMusicWithInstructionActivity.java
+++ app/src/main/java/com/example/pianoforkid/view/activity/PlayMusicWithInstructionActivity.java
@@ -149,7 +149,7 @@ public class PlayMusicWithInstructionActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
 
-        onUp = (int) imageA.getY() + 200;
+        onUp = (int) imageA.getY() + 100;
         onDown = onUp - 100;
         resetAllNote();
     }
